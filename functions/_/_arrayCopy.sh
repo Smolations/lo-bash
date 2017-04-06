8ebecd68eaea60c9deddc840a9ba4872c92286e3
Chris Smola <csmola@tangogroup.com>
2017-04-06 14:39:23 -0600

    (master) bugfix in _arrayCopy

--- functions/_/_arrayCopy.sh
+++ functions/_/_arrayCopy.sh
@@ -34,7 +34,8 @@ function _._arrayCopy {
   local turn=1 arr_copy=$1 arr_orig=$2
 
   if _.isArray $arr_orig; then
-    turn=0 eval "${arr_copy}=( \"\${${arr_orig}[@]}\" )"
+    turn=0
+    eval "${arr_copy}=( \"\${${arr_orig}[@]}\" )"
   else
     eval "${arr_copy}="
   fi
