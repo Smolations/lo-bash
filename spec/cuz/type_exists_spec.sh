669e0f4a0b60e731fc440f541d4239aa9ca9e352
Chris Smola <callmesmola@gmail.com>
2023-07-06 11:55:30 -0400

    adds cuz specs

---
+++ spec/cuz/type_exists_spec.sh
@@ -0,0 +1,23 @@
+xDescribe 'array: _type_exists()'
+  Include SOURCEME
+
+#   setup() {
+#   }
+#
+#   BeforeCall 'setup'
+
+  It ''
+    When call _type_exists
+    The status should be success
+    The value true should equal false
+  End
+
+  Context 'errors'
+    It 'with invalid array name'
+      When call _type_exists
+      The status should be failure
+      The status should equal 1
+      # The output should equal ''
+    End
+  End
+End
